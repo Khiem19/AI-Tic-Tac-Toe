d032f87e8225629b179679d612b15eacc10aac39
Khiem Nguyen <nghoangkhiem19@gmail.com>
2024-08-13 15:47:11 +0200

    base functions

---
+++ game.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <iomanip>
+
+#include "game.h"
+
+using namespace std;
+
+Game::Game() {
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++)
+        {
+            board[i][j] = '-';
+        }
+        
+    }
+}
+
+void Game::printBoard() {
+    cout << "-----------------------";
+    for (int i = 0; i < 3; i++){
+        cout << '\n' << "|";
+        for (int j = 0; j < 3; j++)
+        {
+            cout << setw(3) << board[i][j] << setw(3) << " |";
+        }
+        
+    }
+    cout << "\n ----------------------- \n";
+}
+
+bool Game::gameOver() {
+    if (CheckWin(HUMAN)) return true;
+    else if (CheckWin(AI)) return true;
+
+    bool emptySpace = false;
+    for (int i = 0; i < 3; i++){
+        if(board[i][0] == '-' && board[i][1] == '-' && board[i][2] == '-')
+            emptySpace = true;
+    }
+    return !emptySpace;
+}
+
+bool Game::CheckWin(Player player) {
+    char Playerchar;
+    if(player == HUMAN) Playerchar = human;
+    else Playerchar = ai;
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (board[i][0] == Playerchar && board[i][1] == Playerchar && board[i][2] == Playerchar)
+            return true;
+        if (board[0][i] == Playerchar && board[1][i] == Playerchar && board[2][i] == Playerchar)
+			return true;
+    }
+
+    if (board[0][0] == Playerchar && board[1][1] == Playerchar && board[2][2] == Playerchar) {
+        return true;
+    } else if (board[2][0] == Playerchar && board[1][1] == Playerchar && board[0][2] == Playerchar) {
+        return true;
+    }
+
+    return false;
+}
+
+int Game::score() {
+    if (CheckWin(HUMAN)) return 10;
+    if (CheckWin(AI)) return -10;
+    return 0;
+}
+
---
+++ game.h
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+const char human = 'X';
+const char ai = 'O';
+
+enum Player {HUMAN, AI};
+
+struct Move {
+    int row;
+    int col;
+};
+
+class Game {
+    char board[3][3];
+
+    public:
+        Game();
+
+        void printBoard();
+
+        bool gameOver();
+
+        bool CheckWin(Player player);
+
+        void play();
+
+        void getHumanMove();
+
+        int score();
+
+        Move minimax(char AIboard[3][3]);
+
+        int minsearch(char AIboard[3][3]);
+
+        int maxsearch(char AIboard[3][3]);
+};
